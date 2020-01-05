d79662ed7e7cc558c8544c15b49a4a3ccfb03ef2
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-01-05 17:13:46 -0500

    show all images in list. fixes #8

--- app/admin/content_blocks.rb
+++ app/admin/content_blocks.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register ContentBlock do
-  permit_params :page_id, :content_type_id, :name, :title, :content, images: []
-  config.sort_order = "LOWER(page), LOWER(content_type), LOWER(name), LOWER(title)"
+  permit_params :page_id, :content_type_id, :name, :title, :date, :content, images: []
+  config.sort_order = "LOWER(page), LOWER(content_type), date, LOWER(name), LOWER(title)"
 
   index do
     selectable_column
@@ -9,13 +9,16 @@ ActiveAdmin.register ContentBlock do
     column :page
     column :content_type
     column :name
+    column :date
     column :title
     column :images do |block|
       if image = block.images.first
-        span image_tag(image.representation(resize_to_limit: [100, 100]), alt: block.name)
         if block.images.size > 1
-          br
-          status_tag true, label: " + #{block.images.size - 1}"
+          block.images.each do |image|
+            span image_tag(image.representation(resize_to_limit: [50, 50]), alt: block.name)    
+          end
+        else
+          span image_tag(image.representation(resize_to_limit: [100, 100]), alt: block.name)  
         end
       end
     end
@@ -33,6 +36,7 @@ ActiveAdmin.register ContentBlock do
       row :page
       row :name
       row :content_type
+      row :date
       row :title
       row :images do |block|
         block.images.map do |image|
