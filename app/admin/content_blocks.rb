1b98339ca4fc93b52f3b394d565c87f9adf0a7b5
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-01-05 22:57:43 -0500

    HACK version of replace_on_assign_to_many(false). fixes #9

--- app/admin/content_blocks.rb
+++ app/admin/content_blocks.rb
@@ -54,4 +54,31 @@ ActiveAdmin.register ContentBlock do
   end
 
   form partial: 'form'
+
+
+  # updating a resource that had images caused the prior images to be deleted :(
+  # this behavior was mentioned in https://github.com/rails/rails/pull/36716 but it was supposed to be fixed
+  # we tried setting `config.active_storage.replace_on_assign_to_many false` in config/application.rb, but no dice
+  # assign_attributes just delegates to the same method on object which seems to be right but idk...
+  controller do
+    def update_resource(object, attributes)
+      # update_resource defined in
+      # https://github.com/activeadmin/activeadmin/blob/9a33a081c99d9aa19045d46155ffe5f51ea76710/lib/active_admin/resource_controller/data_access.rb
+
+      # HACK version of replace_on_assign_to_many(false)
+      if images = params[:content_block][:images]
+        images.each do |image|
+          object.images.attach(image)
+        end
+        params[:content_block].delete(:images)
+        Rails.logger.warn "******************* Appending not replacing: object.images.attach(image)"
+      end
+
+      # rest of method is the same as the default implementation
+      object = assign_attributes(object, attributes)
+      run_update_callbacks object do
+        save_resource(object)
+      end
+    end
+  end
 end
